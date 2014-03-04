b16f8cf71920d86daebf4824ef06b3fc663581c3
annie <a.guo.2010@gmail.com>
2014-03-04 16:25:12 +0000

    minor fix

--- c3_reduce.js
+++ c3_reduce.js
@@ -2,6 +2,7 @@ var _=require('underscore')
 
 console.log("==begin");
 
+// reduce_.reduce(list, iterator, memo, [context]) Aliases: inject, foldl 
 console.log("\n\n=alias: reduce");
 var a=_.reduce([1,2,3], function(memo,num){return  memo+num},0);
 console.log(a)
