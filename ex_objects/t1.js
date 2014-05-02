119ec922cacb214ca5968a6392784e4851fd1777
annie <a.guo.2010@gmail.com>
2014-05-02 18:19:54 +0000

    parse a key from a json obj

---
+++ ex_objects/t1.js
@@ -0,0 +1,10 @@
+_=require('underscore')
+
+console.log('=====begin')
+a={".update":{"blecounter":486,"fw_version":null,"hubcounter":292156,"hw_version":null,"time":1399054623799,"value":-93}}
+
+b= _.keys(a);
+console.log(a)
+console.log(b)
+
+
